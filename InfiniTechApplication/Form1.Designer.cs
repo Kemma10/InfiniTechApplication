dd4148d70fc2a2cd794db7711a871718460c72dd
Estelle Alai <estellealai@gmail.com>
2024-06-13 10:08:00 +1200

    Olivia did New, Open, save and Estelle did save as, print and exirt

--- InfiniTechApplication/Form1.Designer.cs
+++ InfiniTechApplication/Form1.Designer.cs
@@ -30,93 +30,102 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsCtrlShiftSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.printCtrlPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.printStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(426, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(568, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "File";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem2,
-            this.toolStripMenuItem3,
-            this.toolStripMenuItem4,
+            this.newStripMenuItem,
+            this.openStripMenuItem,
+            this.saveStripMenuItem,
             this.saveAsCtrlShiftSToolStripMenuItem,
-            this.printCtrlPToolStripMenuItem,
-            this.toolStripMenuItem5});
+            this.printStripMenuItem,
+            this.exitStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
             this.toolStripMenuItem1.Text = "File";
+            // 
+            // newStripMenuItem
+            // 
+            this.newStripMenuItem.Name = "newStripMenuItem";
+            this.newStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.newStripMenuItem.Text = "New                   Ctrl+N";
+            this.newStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // openStripMenuItem
+            // 
+            this.openStripMenuItem.Name = "openStripMenuItem";
+            this.openStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.openStripMenuItem.Text = "Open                 Ctrl+O";
+            this.openStripMenuItem.Click += new System.EventHandler(this.openStripMenuItem_Click);
+            // 
+            // saveStripMenuItem
+            // 
+            this.saveStripMenuItem.Name = "saveStripMenuItem";
+            this.saveStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.saveStripMenuItem.Text = "Save                   Ctrl+S";
+            this.saveStripMenuItem.Click += new System.EventHandler(this.saveStripMenuItem_Click);
             // 
             // saveAsCtrlShiftSToolStripMenuItem
             // 
             this.saveAsCtrlShiftSToolStripMenuItem.Name = "saveAsCtrlShiftSToolStripMenuItem";
-            this.saveAsCtrlShiftSToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
+            this.saveAsCtrlShiftSToolStripMenuItem.Size = new System.Drawing.Size(276, 26);
             this.saveAsCtrlShiftSToolStripMenuItem.Text = "Save As             Ctrl+Shift+S";
+            this.saveAsCtrlShiftSToolStripMenuItem.Click += new System.EventHandler(this.saveAsCtrlShiftSToolStripMenuItem_Click);
             // 
-            // printCtrlPToolStripMenuItem
+            // printStripMenuItem
             // 
-            this.printCtrlPToolStripMenuItem.Name = "printCtrlPToolStripMenuItem";
-            this.printCtrlPToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
-            this.printCtrlPToolStripMenuItem.Text = "Print                  Ctrl+P";
+            this.printStripMenuItem.Name = "printStripMenuItem";
+            this.printStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.printStripMenuItem.Text = "Print                  Ctrl+P";
+            this.printStripMenuItem.Click += new System.EventHandler(this.printStripMenuItem_Click);
             // 
-            // toolStripMenuItem2
+            // exitStripMenuItem
             // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(218, 22);
-            this.toolStripMenuItem2.Text = "New                   Ctrl+N";
-            // 
-            // toolStripMenuItem3
-            // 
-            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(218, 22);
-            this.toolStripMenuItem3.Text = "Open                 Ctrl+O";
-            // 
-            // toolStripMenuItem4
-            // 
-            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(218, 22);
-            this.toolStripMenuItem4.Text = "Save                   Ctrl+S";
+            this.exitStripMenuItem.Name = "exitStripMenuItem";
+            this.exitStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.exitStripMenuItem.Text = "Exit";
+            this.exitStripMenuItem.Click += new System.EventHandler(this.exitStripMenuItem_Click);
             // 
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(426, 420);
+            this.richTextBox1.Size = new System.Drawing.Size(568, 518);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
-            // toolStripMenuItem5
-            // 
-            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
-            this.toolStripMenuItem5.Size = new System.Drawing.Size(218, 22);
-            this.toolStripMenuItem5.Text = "Exit";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 444);
+            this.ClientSize = new System.Drawing.Size(568, 546);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -131,12 +140,12 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem saveAsCtrlShiftSToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem printCtrlPToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem printStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveStripMenuItem;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem exitStripMenuItem;
     }
 }
 
--- InfiniTechApplication/Form1.cs
+++ InfiniTechApplication/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace InfiniTechApplication
 {
@@ -25,6 +26,56 @@ namespace InfiniTechApplication
         private void toolStripTextBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to create a new note and discard the current one?",
+                    "Confirm New Note",
+                    MessageBoxButtons.YesNo
+                );              
+                if (result == DialogResult.Yes) // If yes, clear the richtextbox
+                {
+                    richTextBox1.Clear();
+                }
+                // If the user clicks 'No', do nothing
+            }
+            else
+            {                
+                richTextBox1.Clear(); // If richtextbox is empty, just clear it
+            }
+        }
+
+        private void openStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // Create Open file dialog instance
+            openFileDialog.ShowDialog(); // Show the dialog
+        }
+
+        private void saveStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog(); // Create Save file dialog instance
+            saveFileDialog.ShowDialog(); // Show the dialog
+        }
+
+        private void saveAsCtrlShiftSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog(); // Create Save file dialog instance
+            saveFileDialog.ShowDialog(); // Show the dialog
+        }
+
+        private void printStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog(); //Creates print dialog instance
+            printDialog.ShowDialog(); // Show the dialog
+        }
+
+        private void exitStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //Closes application
         }
     }
 }
